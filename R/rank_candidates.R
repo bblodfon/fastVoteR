c4f89c3a90dce25f104039c048d36b20a0306aa0
john <bblodfon@gmail.com>
2026-04-20 00:42:00 +0200

    refine doc

--- R/rank_candidates.R
+++ R/rank_candidates.R
@@ -7,68 +7,105 @@
 #' see Zobolas et al. (2026).
 #'
 #' @details
-#' This method implements several consensus-based ranking methods, where voters express preferences for candidates.
+#' This method implements several consensus-based ranking methods, where voters
+#' express preferences for candidates.
 #' The input framework considers:
-#' - **Voters**: A list where each element represents the preferences (subsets of candidates) of a single voter.
-#' - **Candidates**: A vector of all possible candidates. This vector is shuffled before processing to enforce random tie-breaking across methods.
-#' - **Weights**: A numeric vector specifying the *influence* of each voter. Equal weights indicate all voters contribute equally; different weights can reflect varying voter importance.
+#' - **Voters**: A list where each element represents the preferences (subsets
+#' of candidates) of a single voter.
+#' - **Candidates**: A vector of all possible candidates. This vector is shuffled
+#' before processing to enforce random tie-breaking across methods.
+#' - **Weights**: A numeric vector specifying the *influence* of each voter.
+#' Equal weights indicate all voters contribute equally; different weights can
+#' reflect varying voter importance.
 #'
-#' The following methods are supported for ranking candidates:
-#' - `"av"`: **Approval Voting (AV)** ranks candidates based on the number of voters approving them.
-#' - `"sav"`: **Satisfaction Approval Voting (SAV)** ranks candidates by normalizing approval scores based on the size of each voter's approval set.
-#' Voters who approve more candidates contribute a lesser score to the individual approved candidates.
-#' - `"seq_pav"`: **Sequential Proportional Approval Voting (PAV)** builds a committee by iteratively maximizing a proportionality-based satisfaction score.
-#' The **PAV score** is a metric that calculates the weighted sum of harmonic numbers corresponding to the number of elected candidates supported by each voter, reflecting the overall satisfaction of voters in a committee selection process.
-#' - `"seq_phragmen"`: **Sequential Phragmen's Rule** selects candidates to balance voter representation by distributing "loads" evenly.
-#' The rule iteratively selects the candidate that results in the smallest increase in voter load.
-#' This approach is suitable for scenarios where a balanced representation is desired, as it seeks to evenly distribute the "burden" of representation among all voters.
+#' The following methods are supported:
+#' - `"av"`: **Approval Voting (AV)** ranks candidates based on the number of
+#' voters approving them.
+#' - `"sav"`: **Satisfaction Approval Voting (SAV)** ranks candidates by
+#' normalizing approval scores based on the size of each voter's approval set.
+#' Voters who approve more candidates contribute a lesser score to the individual
+#' approved candidates.
+#' - `"seq_pav"`: **Sequential Proportional Approval Voting (PAV)** builds a
+#' committee by iteratively maximizing a proportionality-based satisfaction score.
+#' The **PAV score** is a metric that calculates the weighted sum of harmonic
+#' numbers corresponding to the number of elected candidates supported by each
+#' voter, reflecting the overall satisfaction of voters in a committee selection
+#' process.
+#' - `"seq_phragmen"`: **Sequential Phragmen's Rule** selects candidates to
+#' balance voter representation by distributing "loads" evenly.
+#' The rule iteratively selects the candidate that results in the smallest
+#' increase in voter load.
+#' This approach is suitable for scenarios where a balanced representation is
+#' desired, as it seeks to evenly distribute the "burden" of representation among
+#' all voters.
 #'
 #' All methods have weighted versions which consider voter weights.
 #'
-#' To allow for method-agnostic comparisons of rankings, we calculate the **borda scores** for each method as:
+#' To allow for method-agnostic comparisons of rankings, we calculate the
+#' **borda scores** for each method as:
 #' \deqn{s_{borda} = (p - i) / (p - 1)}
-#' where \eqn{p} is the total number of candidates, and \eqn{i} is the candidate's rank.
+#' where \eqn{p} is the total number of candidates, and \eqn{i} is the
+#' candidate's rank.
 #'
-#' @param voters (`list`) \cr
-#'   A list of subsets, where each subset contains the candidates approved or selected by a voter.
-#' @param candidates (`character`) \cr
-#'   A vector of all candidates to be ranked.
-#' @param weights (`numeric`) \cr
-#'   A numeric vector of weights representing each voter's influence.
-#'   Larger weight, higher influence.
-#'   Must have the same length as `voters`.
-#'   If `NULL` (default), all voters are assigned equal weights of 1, representing equal influence.
+#' @param voters (`list()`) \cr
+#'  A list of subsets (`character` vectors), where each subset contains the
+#'  (assumed unique) candidates approved or selected by a voter.
+#' @param candidates (`character()`) \cr
+#'  A vector of all candidates to be ranked.
+#' @param weights (`numeric()|NULL`) \cr
+#'  A numeric vector of non-negative weights representing each voter's influence.
+#'  Larger weight, higher influence.
+#'  Must have the same length as `voters`.
+#'  If `NULL` (default), all voters are assigned equal weights of 1, representing
+#'  equal influence.
 #' @param committee_size (`integer(1)`)\cr
-#'   Number of top candidates to include in the ranking. Default (`NULL`) includes all candidates.
-#'   For sequential methods such as `"seq_pav"` and `"seq_phragmen"`, this parameter can speed up computation by limiting the selection process to only the top N candidates, instead of generating a complete ranking.
-#'   In other methods (e.g., `"sav"` or `"av"`), this parameter simply filters the final output to include only the top N candidates from the complete ranking.
+#'  Number of top candidates to include in the ranking.
+#'  Default (`NULL`) includes all candidates.
+#'  For sequential methods such as `"seq_pav"` and `"seq_phragmen"`, this parameter
+#'  can speed up computation by limiting the selection process to only the top N
+#'  candidates, instead of generating a complete ranking.
+#'  In other methods (e.g., `"sav"` or `"av"`), this parameter simply filters
+#'  the final output to include only the top N candidates from the complete
+#'  ranking.
 #' @param method (`character(1)`) \cr
-#'   The ranking voting method to use. Must be one of: `"av"`, `"sav"`, `"seq_pav"`, `"seq_phragmen"`.
-#'   See Details.
+#'  The ranking voting method to use. Must be one of: `"av"`, `"sav"`, `"seq_pav"`,
+#'  `"seq_phragmen"`. See Details.
 #' @param borda_score (`logical(1)`) \cr
-#'   Whether to calculate and include Borda scores in the output. See Details.
-#'   Default is `TRUE`.
+#'  Whether to calculate and include Borda scores in the output. See Details.
+#'  Default is `TRUE`.
 #' @param shuffle_candidates (`logical(1)`) \cr
-#'   Whether to shuffle the candidates randomly before computing the ranking.
-#'   Shuffling ensures consistent random tie-breaking across methods and prevents
-#'   deterministic biases when candidates with equal scores are encountered.
-#'   Default is `TRUE`. Set to `FALSE` if deterministic ordering of candidates is preferred.
+#'  Whether to shuffle the candidates randomly before computing the ranking.
+#'  Shuffling ensures random tie-breaking across methods and prevents
+#'  deterministic biases when candidates with equal scores are encountered.
+#'  Default is `TRUE`.
 #'
 #' @return A [data.table::data.table] with columns:
 #' - `"candidate"`: Candidate names.
-#' - `"score"`: Scores assigned to each candidate based on the selected method (if applicable).
-#' - `"norm_score"`: Normalized scores (if applicable), scaled to the range \eqn{[0,1]}, which can be loosely interpreted as **selection probabilities** (see Meinshausen et al. (2010) for an example in Machine Learning research where the goal is to perform stable feature selection).
-#' - `"borda_score"`: Borda scores for method-agnostic comparison, ranging in \eqn{[0,1]}, where the top candidate receives a score of 1 and the lowest-ranked candidate receives a score of 0.
+#' - `"score"`: Scores assigned to each candidate based on the selected method
+#' (if applicable).
+#' - `"norm_score"`: Normalized scores (if applicable), scaled to the range
+#' \eqn{[0,1]}, which can be loosely interpreted as **selection probabilities**
+#' (see Meinshausen et al. (2010) for an example in Machine Learning research
+#' where the goal is to perform stable feature selection).
+#' - `"borda_score"`: Borda scores for method-agnostic comparison, ranging in
+#' \eqn{[0,1]}, where the top candidate receives a score of 1 and the lowest-ranked
+#' candidate receives a score of 0.
 #'
-#' Candidates are ordered by decreasing `"score"`, or by `"borda_score"` if the method returns only rankings.
+#' Candidates are ordered by decreasing `"score"`, or by `"borda_score"` if the
+#' method returns only rankings.
 #'
 #' @references
 #'
-#' Lackner M, Skowron P (2023). *Multi-Winner Voting with Approval Preferences*. Springer Nature, 121 p. \doi{10.1007/978-3-031-09016-5}.
+#' Lackner M, Skowron P (2023). *Multi-Winner Voting with Approval Preferences*.
+#' Springer Nature, 121 p. \doi{10.1007/978-3-031-09016-5}.
 #'
-#' Zobolas J, George A, Lopez A, Fischer S, Becker M, Aittokallio T (2026). "Prognostic biomarker discovery in pancreatic cancer through hybrid ensemble feature selection and multi-omics data." *BioData Mining*. \doi{10.1186/s13040-026-00546-0}.
+#' Zobolas J, George A, Lopez A, Fischer S, Becker M, Aittokallio T (2026).
+#' "Prognostic biomarker discovery in pancreatic cancer through hybrid ensemble
+#' feature selection and multi-omics data." *BioData Mining*. \doi{10.1186/s13040-026-00546-0}.
 #'
-#' Meinshausen N, Buhlmann P (2010). "Stability Selection." *Journal of the Royal Statistical Society Series B: Statistical Methodology*, 72(4), 417-473. \doi{10.1111/J.1467-9868.2010.00740.X}.
+#' Meinshausen N, Buhlmann P (2010). "Stability Selection." *Journal of the Royal
+#' Statistical Society Series B: Statistical Methodology*, 72(4), 417-473.
+#' \doi{10.1111/J.1467-9868.2010.00740.X}.
 #'
 #' @examples
 #' # 5 candidates
