25778b8d92f64cfc219d80153eb84c993febf6fd
john <bblodfon@gmail.com>
2026-05-15 17:04:22 +0200

    small doc update

--- man-roxygen/param_committee_size.R
+++ man-roxygen/param_committee_size.R
@@ -1,3 +1,3 @@
 #' @param committee_size (`integer(1)|NULL`)\cr
-#'  Number of top candidates to include in the ranking.
-#'  Default (`NULL`) includes all candidates.
+#'  Number of top-ranked candidates to return.
+#'  Default (`NULL`) returns all candidates.
